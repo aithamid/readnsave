47caa3cdc23cb8048cbe85efa6a4359cc622b6bf
Ait Hamid Adam <aithamidbusiness@gmail.com>
2025-02-23 18:57:06 +0100

    test(Init): :memo: Test Rest API with a list of books as variable

--- backend/hello.go
+++ backend/hello.go
@@ -22,7 +22,7 @@ var books = []book{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", CurrentPage: 56, TotalPages: 100},
 }
 
-// getBooks responds with the list of all albums as JSON.
+// getBooks responds with the list of all books as JSON.
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
@@ -38,10 +38,55 @@ func getBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
 }
 
+func postBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func putBook(c *gin.Context) {
+	id := c.Param("id")
+	var updatedBook book
+
+	if err := c.BindJSON(&updatedBook); err != nil {
+		return
+	}
+
+	for i, a := range books {
+		if a.ID == id {
+			books[i] = updatedBook
+			c.IndentedJSON(http.StatusOK, updatedBook)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+}
+
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+	for i, a := range books {
+		if a.ID == id {
+			books = append(books[:i], books[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "book deleted"})
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", getBook)
+	router.POST("/books", postBook)
+	router.PUT("/books/:id", putBook)
+	router.DELETE("/books/:id", deleteBook)
 
 	router.Run("localhost:8080")
 }
